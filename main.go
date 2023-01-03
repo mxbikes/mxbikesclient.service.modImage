d07114d37c76a1adf645d8181a8af24d5bc102a9
RikVanHaaren <contact@rikvanhaaren.com>
2023-01-03 21:48:04 +0100

    remove host

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 
 var (
 	logLevel       = getEnv("LOG_LEVEL", "info")
-	port           = getEnv("PORT", "localhost:4092")
+	port           = getEnv("PORT", ":4092")
 	minioHost      = getEnv("MINIO_HOST", "host.docker.internal:9001")
 	minioAccessKey = getEnv("MINIO_ACCESKEY", "fcR98JVhqBHP2laQ")
 	minioSecret    = getEnv("MINIO_SECRET", "7xMpT01cPq1O1QpWktOjBDjsgZdccPoL")
